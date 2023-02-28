8a92186484f8c2252eabacb762f337b2c43f996f
Aman Singh <119652332+Aman-ABJIMA@users.noreply.github.com>
2023-02-28 18:41:39 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 var date,time;
+const myInterval = setInterval(setColor,1000);
+
+function setColor() {
+  let x = document.getElementById("A2");
+  x.style.color = x.style.color == "white" ? "red" : "white";
+}
+
+function stopColor() {
+  clearInterval(myInterval);
+}
 setInterval(() => {
 var today = new Date();
 date = today.getDate() + '/'+(today.getMonth()+1)+'/'+today.getFullYear();
